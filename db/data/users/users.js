fb4d020c772680a2d78b678c82163a3830854c70
AJ <aj@smartnvm.com>
2022-02-04 22:05:30 -0500

    - add test users data

---
+++ db/data/users/users.js
@@ -0,0 +1,18 @@
+const { ObjectId } = require("bson");
+
+//users collection
+module.exports = [
+   {
+    id: 1,
+    name: "aj",
+    email: "aj@smartnvm.com",
+    hash: "$2a$10$N3BCEpGlU3ImsPgNPdaw7OkTN7IADKJ48K.GBbn5jV.PkfUkzeEqe"
+  },
+  {
+    id: 2,
+    name: "Clement",
+    email: "clement.shum@speer.io",
+    hash: "$2a$10$N3BCEpGlU3ImsPgNPdaw7OkTN7IADKJ48K.GBbn5jV.PkfUkzeEqe"
+  },
+
+];
