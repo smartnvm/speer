7c937781ac281396ba5d3a612884dbd6d10460e9
AJ <aj@smartnvm.com>
2022-02-05 17:39:12 -0500

    - add dB users seed data

---
+++ db/seed/users/users.js
@@ -0,0 +1,20 @@
+const { ObjectId } = require("bson");
+
+//users collection
+module.exports = [
+   {
+    id: 1,
+    name: "aj",
+    username: "aj@smartnvm.com",
+    hash: "$2b$10$p/CQ0qb5wN2WLMNvanxhV.kVg74c9xixlALRQpvY7cQCpr1TyLhhO",
+    password: 'password'
+  },
+  {
+    id: 2,
+    name: "Clement",
+    username: "clement.shum@speer.io",
+    hash: "$2b$10$p/CQ0qb5wN2WLMNvanxhV.kVg74c9xixlALRQpvY7cQCpr1TyLhhO",
+    password: 'password'
+  },
+
+];
