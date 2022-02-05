6d3c2a17c0075ad6bfb127cf0ad7ca98e48a63ed
AJ <aj@smartnvm.com>
2022-02-05 17:29:08 -0500

    - add user authentication POST /api/login

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,29 +1,42 @@
 
-const testUsers = require("../db/data/users/users");
+const bcrypt = require('bcrypt')
+
+const testUsersdB = require("../db/test_data/users/users");
+
+async function authenticateUser(username, password, testUsersdB) {
+  if (testUsersdB[username]) {
+    const hashMatch = await bcrypt.compare(password, testUsersdB[username].hash);
+    const match = password === testUsersdB[username].password
+    return match;
+  }
+  return false;
+}
+
 
 module.exports = (router, dbo) => {
 
   router.get("/login", (req, res) => {
     //check if we are already logged in
-    const userId ="0";
-    console.log("login:", userId);
-
-    if (!userId) return res.json({ err: 401, msg: "user not logged in" });
-
-    const user = testUsers[userId];
+    const userId = req.session.username;
+    console.log("session username:", userId);
+    if (!userId) {
+      return res.json({ err: 401, msg: "user not logged in" });
+    }
+    const user = testUsersdB[userId];
     res.json(user);
   });
 
   router.post("/login", (req, res) => {
-    res.json({cod: 200, POST:'/login'});
+    const { username, password } = req.body;
+    authenticateUser(username, password, testUsersdB)
+      .then(auth => {
+        console.log('------[auth?]---------', auth)
+        return res.json({ auth });
+      })
   });
 
   router.post("/register", (req, res) => {
-    res.json({cod: 200, POST:'/register'});
-  });
-
-  router.get("/register", (req, res) => {
-    res.json({cod: 200, GET:'/register'});
+    res.json({ cod: 200, POST: '/register' });
   });
 
 
