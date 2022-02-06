e0c2ebc954357601fe1f18368a1aa9441e2cb5da
AJ <aj@smartnvm.com>
2022-02-06 03:26:40 -0500

    - reset tweet seed/test data

--- routes/debugRoutes.js
+++ routes/debugRoutes.js
@@ -1,4 +1,5 @@
 const testUsers = require('../db/seed/users/users')
+const tweets = require('../db/seed/tweets/tweets')
 
 module.exports = (router, dbo, API) => {
 
@@ -15,8 +16,12 @@ module.exports = (router, dbo, API) => {
   router.get("/db_reset", (req, res) => {
     const dbConn = dbo.getDb();
     dbConn.collection("users").deleteMany({})
-    return dbConn.collection("users").insertMany(testUsers)
-    .then(res.json({cod: 200, cmd: 'db_reset', testUsers}))
+    dbConn.collection("users").insertMany(testUsers)
+
+    dbConn.collection("tweets").deleteMany({})
+    dbConn.collection("tweets").insertMany(tweets)
+
+    res.json({ cod: 200, cmd: 'db_reset', testUsers, tweets })
 
   });
 
