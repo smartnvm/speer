d6b2c39ec9944b0d3c918fe9d530a47b34605765
AJ <aj@smartnvm.com>
2022-02-06 03:29:52 -0500

    - rename id to _id to avoid random _id on dB reset

--- db/seed/users/users.js
+++ db/seed/users/users.js
@@ -3,14 +3,14 @@ const { ObjectId } = require("bson");
 //users collection
 module.exports = [
    {
-    id: 1,
+    _id: ObjectId('61ff81d20f7133dc693200e4'),
     name: "aj",
     username: "aj@smartnvm.com",
     hash: "$2b$10$p/CQ0qb5wN2WLMNvanxhV.kVg74c9xixlALRQpvY7cQCpr1TyLhhO",
     password: 'password'
   },
   {
-    id: 2,
+    _id: ObjectId('61ff81d20f7133dc693200e5'),
     name: "Clement",
     username: "clement.shum@speer.io",
     hash: "$2b$10$p/CQ0qb5wN2WLMNvanxhV.kVg74c9xixlALRQpvY7cQCpr1TyLhhO",
