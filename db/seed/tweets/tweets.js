afe46301b133705014c4669fc32bd017fbcdd0e3
AJ <aj@smartnvm.com>
2022-02-06 05:18:26 -0500

    - add more tweet seed/test data

--- db/seed/tweets/tweets.js
+++ db/seed/tweets/tweets.js
@@ -22,5 +22,27 @@ module.exports = [
     timestamp: new Date(Date.now() + ~~(Math.random()*3600*24*2*1000)),
   },
 
+  {
+    id: 4,
+    author: 'Bot',
+    body: "hello world - ",
+    authorId: '61ff982feda21347c358beef',
+    timestamp: "2022-02-06T09:56:58.976Z"
+  },
+  {
+    id: 5,
+    author: 'Poet',
+    body: "lorem ipsum blue moon",
+    authorId: '61ff982feda21347c358a550',
+    timestamp: "2022-02-06T09:57:00.470Z"
+  },
+  {
+    id: 6,
+    author: 'The Eagles',
+    body: "up ahead in the distance, i saw a shimmering light... you can check anytime you want... but you can never leaaaaaave",
+    authorId: '61ff982feda21347c3589db9',
+    timestamp: "2022-02-06T09:57:02.428Z"
+  },
+
 
 ];
