5e10e6df34b8a873c5d056870e418b93fcd3e786
AJ <aj@smartnvm.com>
2022-02-06 05:21:06 -0500

    - finish POST /api/tweets/new and add GET endpoint

--- routes/tweetsRoutes.js
+++ routes/tweetsRoutes.js
@@ -5,60 +5,45 @@ const tweets = require("../db/test_data/tweets/tweets");
 const db = require('../database');
 
 console.log(tweets)
-const {authenticateUser , createUser} = require('../utils/usersUtil')
+const { authenticateUser, createUser } = require('../utils/usersUtil')
 
 
 module.exports = (router, dbo) => {
 
-  router.get("/", (req, res) => {
+  //create a new tweet
+  router.post("/new", (req, res) => {
     //check if we are already logged in
-    const userId = req.session.userid;
-    console.log("session username:", userId);
-    if (!userId) {
+    const authorId = req.session.userid;
+    if (!authorId) {
       return res.json({ cod: 403, msg: "Forbidden! Please login first!" });
     }
-    console.log(userId)
-    // const tweets = tweets[userId];
-    res.json({cod:200, tweetsdB});
+
+    const tweet = { ...req.body, authorId, timestamp: new Date(Date.now()) }
+    const database = dbo.getDb()
+
+    database.collection("tweets").insertOne(tweet);
+    res.json({ cod: 200, tweet });
+
   });
 
-  router.post("/post", (req, res) => {
+
+  //get user's tweets
+  router.get("/", (req, res) => {
     //check if we are already logged in
-    const userId = req.session.userid;
-    if (!userId) {
-      return res.json({ err: 403, msg: "Forbidden! Please login first!" });
+    const authorId = req.session.userid;
+    if (!authorId) {
+      return res.json({ cod: 403, msg: "Forbidden! Please login first!" });
     }
 
-    const { author, body } = req.body;
     const database = dbo.getDb()
-    database
-      .collection("tweets")
-      .find({ username })
-      .toArray((err, dbRes) => {
+
+    database.collection("tweets").find({ authorId })
+      .toArray((err, tweets) => {
         if (err) throw err;
-        const user = dbRes[0]
-        if (!user) {
-          console.log({ cod: 404, auth: false, msg: `user ${username} not found` })
-          res.json({ cod: 404, auth:false, msg: `user ${username} not found` });
-          return
-        }
-
-        authenticateUser(password, user)
-          .then(auth => {
-            //create session cookie
-
-            if (!auth) {
-              req.session.userid = null
-              return res.json({ cod: 403, auth, msg: 'incorrect password' });
-            }
-
-            req.session.userid = user._id
-            console.log('------[auth?]---------\n', req.session.userid)
-            return res.json({ cod: 200, auth, msg: `hello ${user.username}` });
-          })
+        res.json({ cod: 20,  count:tweets.length, tweets });
       })
 
-  })
+  });
 
 
 
